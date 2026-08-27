c8103d592495e7db0ef3f4188b0138eb25b28203
jaweria <jaweriaaslam1@gmail.com>
2026-08-27 11:20:41 +1000

    update theoutput.tf file

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "load_balancer_ip" {
   description = "Public IP address of the global HTTP load balancer."
-  value       = module.loadbalancer.global_ip_address
+  value       = module.loadbalancer.public-ip
 }
 
 output "instance_group_name" {
